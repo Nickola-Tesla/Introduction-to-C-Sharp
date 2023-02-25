87ca0a89c8ac066e76303c56d9b3035ddec03fc7
Nickolay <urraks@yandex.ru>
2023-02-25 18:42:31 +0300

    Added two homework tasks

--- Семинар1/Program.cs
+++ Семинар1/Program.cs
@@ -16,9 +16,11 @@ Console.WriteLine(result);
 // Пример применения ГСЧ
 int numberA = new Random().Next(0, 100);
 Console.WriteLine(numberA);*/
-
-int a = 7;
-int b = 7;
+Console.WriteLine("Задача № 2");
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
 if (a > b)
 {
    Console.WriteLine("Болшее значение = " + a);
@@ -29,7 +31,22 @@ if (a > b)
 {
     Console.WriteLine("Значения равны: " + a + " = " + b);
 }
+Console.WriteLine("Задача № 4");
+Console.Write("Введите 3 числа через пробел: ");
+string[] array1 = Console.ReadLine()!.Split(" ");
+int i = 0, size = array1.Length;
+int max = Convert.ToInt32(array1[0]);
 
+while (i < size)
+{
+    if (max < Convert.ToInt32(array1[i]))
+    {
+        max = Convert.ToInt32(array1[i]);
+    }
+    i++;
+}
+Console.WriteLine(max);
+Console.WriteLine("Задача № 4");
 
 
 
