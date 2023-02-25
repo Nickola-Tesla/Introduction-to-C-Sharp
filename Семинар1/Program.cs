53fd18a600c388a2c0bc1638eda00462f53d508a
Nickolay <urraks@yandex.ru>
2023-02-25 17:04:58 +0300

    First commit

---
+++ Семинар1/Program.cs
@@ -0,0 +1,44 @@
+﻿// Практика по лекции 1
+//Пример 1 - Ввод имени и вывод приветсвия
+/*Console.WriteLine("Введите ваше имя ");
+string username = Console.ReadLine();
+Console.WriteLine("Привет, " );
+Console.WriteLine(username);
+Console.Write("Привет, " );
+Console.Write(username);
+// Пример 2 - программа сложения 2-х чисел
+int numberA = 3;
+int numberB = 5;
+Console.WriteLine(numberA + numberB);
+//или 
+int result = numberA + numberB;
+Console.WriteLine(result);
+// Пример применения ГСЧ
+int numberA = new Random().Next(0, 100);
+Console.WriteLine(numberA);*/
+
+int a = 7;
+int b = 7;
+if (a > b)
+{
+   Console.WriteLine("Болшее значение = " + a);
+}else if(b > a)
+{
+    Console.WriteLine("Болшее значение = " + b);
+}else
+{
+    Console.WriteLine("Значения равны: " + a + " = " + b);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+//Console.ReadLine()
