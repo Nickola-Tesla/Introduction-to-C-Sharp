480cd5b13d827ee53c7c1a3bb4d6cf966b887ea7
Nickolay <urraks@yandex.ru>
2023-03-10 22:11:03 +0300

    added homework solutions to the 4th webinar!

---
+++ Webinar4/HomeWork/task2/Program.cs
@@ -0,0 +1,25 @@
+﻿/*Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.*/
+int InputNumberWithMessage(string message)
+{
+    System.Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SummingTheDigitsOfTheEnteredNumber(int number)
+{
+    int result = 0;
+    if (number < 0)
+    {
+        number *= -1;
+    }
+    while (number > 0)
+    {
+        result += number%10;
+        number /= 10;
+    }
+    return result;
+}
+int number = InputNumberWithMessage("Введите число : ");
+
+Console.WriteLine("Сумма цифр введенного числа = " + SummingTheDigitsOfTheEnteredNumber(number));
+
