593a3855342b6aabd93457ada39533283aa96e03
Nickolay <urraks@yandex.ru>
2023-03-11 10:37:49 +0300

    an alternative solution has been added to the homework!

--- Webinar3/HomeWork/task2/Program.cs
+++ Webinar3/HomeWork/task2/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 /*Напишите программу, которая принимает на вход координаты двух точек 
 и находит расстояние между ними в 3D пространстве.*/
-Console.Write("Введите координату X точка a: ");
+/*Console.Write("Введите координату X точка a: ");
 double Xa = Convert.ToDouble(Console.ReadLine());
 Console.Write("Введите координату Y точка a: ");
 double Ya = Convert.ToDouble(Console.ReadLine());
@@ -19,4 +19,30 @@ double Zb = Convert.ToDouble(Console.ReadLine());
 //double lengthX = Math.Pow(Xa - Xb, 2);
 //double lengthY = Math.Pow(Ya - Yb, 2);
 double result = Math.Round(Math.Sqrt(Math.Pow(Xa - Xb, 2) + Math.Pow(Ya - Yb, 2) + Math.Pow(Za - Zb, 2)), 2);
-Console.WriteLine("расстояние между a и b в 3D пространстве = " + result);
+Console.WriteLine("расстояние между a и b в 3D пространстве = " + result);*/
+double GetImputNumber(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+(double X, double Y, double Z) GetPointIn3DArea()
+{
+    double X = GetImputNumber("Введите координату X");
+    double Y = GetImputNumber("Введите координату Y");
+    double Z = GetImputNumber("Введите координату Z");
+
+    return (X,Y,Z);
+}
+double GetDistanceIn3DArea((double X, double Y, double Z) point1, (double X, double Y, double Z) point2)
+{
+    double lengthX = Math.Pow(point2.X - point1.X, 2);
+    double lengthY = Math.Pow(point2.Y - point1.Y, 2);
+    double lengthZ = Math.Pow(point2.Z - point1.Z, 2);
+    return Math.Sqrt(lengthX + lengthY + lengthZ);
+}
+
+(double X, double Y, double Z) point1 = GetPointIn3DArea();
+(double X, double Y, double Z) point2 = GetPointIn3DArea();
+
+Console.WriteLine("Расстояние между точками: " + Math.Round(GetDistanceIn3DArea(point1, point2)), 2);
